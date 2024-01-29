945d4857d645b2529b42a50b95e8ab30178379f6
Alec Z <az94587@gmail.com>
2024-01-28 17:08:00 -0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -405,9 +405,11 @@ public class Player : MonoBehaviour
         {
             g.GetComponent<Collectable>().Respawn(delay);
         }
+        yield return new WaitForSeconds(delay);
+        
         spriteRenderer.enabled = false;
         yield return new WaitForSeconds(delay);
-
+        
         transform.position = respawnPos;
         spriteRenderer.enabled = true;
         
